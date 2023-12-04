527bd74962b37afff211b2de4044d6005d0437e5
relt <haxrelt@gmail.com>
2023-12-04 03:47:24 +0300

    add a message transport between main & child processes

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -1,34 +1,51 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#define MAX_LENGTH 256
 
 int main() {
-  int fd[2];
+  int fd[2]; // fd[0] - read  fd[1] - write
   pid_t pid;
-  char str[] = "char\n";
-  char buf[80];
 
   pipe(fd);
   pid = fork();
 
   if (pid == -1) {
-    return -1;
-  } else if (pid == 0) { // fork-process
-    close(fd[0]);
+    perror("cannot fork a process");
+    exit(errno);
+  }
 
-    write(fd[1], str, strlen(str) + 1);
+  if (pid == 0) { // fork-process
+    close(fd[0]); // closing read fd
+
+    char buf[MAX_LENGTH];
+
+    printf("Enter a message: ");
+    scanf("%s", buf);
+
+    FILE *file = fopen("output.txt", "a");
+    if (file == NULL) {
+      printf("cannot open file\n");
+      exit(-1);
+    }
+
+    fwrite(buf, strlen(buf), 1, file);
+    fwrite("\n", strlen("\n"), 1, file);
+    write(fd[1], buf, strlen(buf) + 1); // writing to fd[1]
+
+    fclose(file);
+
     exit(0);
+  } else {        // main-process
+    close(fd[1]); // closing write fd
 
-  } else { // current-process
-    close(fd[1]);
+    char buf[MAX_LENGTH];
 
-    read(fd[0], buf, sizeof(buf));
-
-    printf("Recieved string: %s", buf);
-
-    exit(0);
+    read(fd[0], buf, sizeof(buf));       // reading from fd[0]
+    printf("Written string: %s\n", buf); // printing result
   }
 
   return 0;
